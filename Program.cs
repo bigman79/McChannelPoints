5b43544865dd4b5abc62c5b7127eeeeef18141ca
bigman79 <64438880+bigman79@users.noreply.github.com>
2020-11-12 23:24:14 -0800

    fuck

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ namespace McChannelPoints
     {
         private static string _botName = "joto_is_here";
         private static string _broadcasterName = "joto_is_here";
-        private static string _twitchOAuth = "oauth:2lt3uyl8a4cvccjkntnsmfqsuim4vk";
+        private static string _twitchOAuth = "oauth:nv302ng19xbh8why4do4bwjhtsgzlc";
         static void Main(string[] args)
         {
             IrcClient client = new IrcClient("irc.twitch.tv", 6667, _botName, _twitchOAuth, _broadcasterName);
-
+            SocketClient socket = new SocketClient();
+            
 
             var pinger = new Pinger(client);
             pinger.Start();
--- SocketClient.cs
+++ SocketClient.cs
@@ -14,9 +14,9 @@ namespace McChannelPoints
     {
         WebSocket socket;
         public string TwitchID = "118542232";
-        public string TwitchOath = "oj8pip257e3d79ujyymfqum7m7rfz1"
-        private Thread sender;
-        public void SocketPinger(We)
+        public string TwitchOath = "ilo3sj0v34g0x8sljrzdq5de81lm2j";
+        
+        public void SocketPinger(WebSocket socket)
         {
             socket = this.socket;
             new Thread(new ThreadStart(Run)) { IsBackground = true }.Start();
@@ -24,22 +24,22 @@ namespace McChannelPoints
         }
 
 
-        public SocketClient(IrcClient client)
+        public SocketClient()
         {
 
             socket = new WebSocket("wss://pubsub-edge.twitch.tv");
             socket.OnOpen += SocketConnect;
             socket.OnMessage += SocketMessage;
             socket.Connect();
-
-
+            SocketPinger(socket);
+            
         }
 
         public void SocketConnect(object sender, EventArgs e)
         {
             socket.Send("{\"type\": \"PING\"}");
-            socket.Send($"{{\"type\": \"LISTEN\", \"nonce\": \"ChannelPoints\", \"dara\": [\"channel-points-channel-v1.{TwitchID}\",\"channel-bits-event-v2.{TwitchID}\",\"channel-subscribe-events-v1.{TwitchID}\"],\"auth_token\": \"{TwitchOath}\"}}}}");
-            new SocketPinger(socket);
+            socket.Send($"{{\"type\": \"LISTEN\", \"nonce\": \"ChannelPoints\", \"data\": {{\"topics\": [\"channel-points-channel-v1.{TwitchID}\",\"channel-bits-event-v2.{TwitchID}\",\"channel-subscribe-events-v1.{TwitchID}\"],\"auth_token\": \"{TwitchOath}\"}}}}");
+            
         }
 
         public void SocketMessage(object sender, MessageEventArgs e)
