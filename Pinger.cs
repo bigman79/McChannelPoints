d10267f3bdb8a301c6b3000bb800643a507c26ba
bigman79 <64438880+bigman79@users.noreply.github.com>
2020-11-12 17:04:43 -0800

    bruh

---
+++ Pinger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace McChannelPoints
+{
+    public class Pinger
+    {
+        
+        private IrcClient client;
+        private Thread sender;
+
+        public Pinger(IrcClient client)
+        {
+            this.client = client;
+            
+        }
+
+        public void Start()
+        {
+            sender.IsBackground = true;
+            sender.Start();
+        }
+
+        private void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Sending PING");
+                client.SendIrcMessage("PING irc.twitch.tv");
+                Thread.Sleep(TimeSpan.FromMinutes(5));
+                Console.WriteLine("Sent PING");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,32 @@
 
 namespace McChannelPoints
 {
+
     class Program
     {
+        private static string _botName = "joto_is_here";
+        private static string _broadcasterName = "joto_is_here";
+        private static string _twitchOAuth = "oauth:2lt3uyl8a4cvccjkntnsmfqsuim4vk";
         static void Main(string[] args)
         {
-            
+            IrcClient client = new IrcClient("irc.twitch.tv", 6667, _botName, _twitchOAuth, _broadcasterName);
+
+
+            var pinger = new Pinger(client);
+            pinger.Start();
+
+            while (true)
+            { 
+                var message = client.ReadMessage();
+                Console.WriteLine($"Message: {message}");
+            }
         }
-    }
-}
+
+        
+    
+    
+    
+    }}
+
+
+   
